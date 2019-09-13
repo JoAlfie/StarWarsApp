6a773881dcee79c6c89b65ad11417da7adc0a887
Jo Wimborne <jo.wimborne@thinkorchard.com>
2019-09-13 17:29:12 +0100

    Display components on index screen (no imported data)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
+import { graphql } from "gatsby"
+import FilmList from "../components/films/filmList"
+import CharacterList from "../components/characters/characterList"
 
-export default () => <div>Hello world!</div>
+class StarWarsApp extends React.Component {
+	render() {
+		const {
+			swapi: { allFilms, allPersons },
+		} = this.props.data
+
+		return (
+			<div className="star-wars-app">
+				<div className="stars"></div>
+				<div className="twinkling"></div>
+				<div className="content">
+					<h2 className="section__title">Films</h2>
+					<FilmList films={allFilms} />
+
+					<h2 className="section__title">Characters</h2>
+					<CharacterList characters={allPersons} />
+				</div>
+			</div>
+		)
+	}
+}
+
+export default StarWarsApp
