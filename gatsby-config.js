ffd45f4e18f06e70664cf5cea647c5f5dadd46fc
Jo Alfie Wimborne <jowimborne@gmail.com>
2019-09-13 18:23:20 +0100

    Installed Gatsby Source Graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+	plugins: [
+		{
+			resolve: "gatsby-source-graphql",
+			options: {
+				// This type will contain remote schema Query type
+				typeName: "SWAPI",
+				// This is field under which it's accessible
+				fieldName: "swapi",
+				// Url to query from
+				url: "https://api.graphcms.com/simple/v1/swapi",
+			},
+		},
+	],
 }
